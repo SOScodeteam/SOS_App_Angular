50e1bf481a881e52b543f3824a2b03b716c1c8a3
Josh Sweeney <jsweens13@gmail.com>
2020-10-08 09:45:50 -0500

    Auth now working. Changed from Redirect to popup.

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,91 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { auth } from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+import { Observable, of } from 'rxjs';
+import { switchMap, take, map } from 'rxjs/operators';
+import { DbService } from './db.service';
+
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+import { Platform } from '@ionic/angular';
+import { LoadingController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  user$: Observable<any>;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private db: DbService,
+    private router: Router,
+    private storage: Storage,
+    private gplus: GooglePlus,
+    private platform: Platform,
+    private loadingController: LoadingController
+  ) { 
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap(user => (user ? db.doc$(`users/${user.uid}`) : of(null)))
+    );
+
+      //this.handleRedirect();
+
+  }
+
+  async anonymousLogin(){
+    const credential = await this.afAuth.signInAnonymously();
+    return await this.updateUserData(credential.user);
+  }
+
+  private updateUserData({uid, email, displayName, photoURL, isAnonymous})  {
+    const path = `users/${uid}`;
+
+    const data = {
+      uid,
+      email,
+      displayName,
+      photoURL,
+      isAnonymous
+    };
+
+    return this.db.updateAt(path, data)
+  }
+
+  async logOut() {
+    await this.afAuth.signOut();
+    return this.router.navigate(['/']);
+  }
+
+  async uid() {
+    return this.user$
+      .pipe(
+        take(1),
+        map(u => u && u.uid)
+      )
+      .toPromise();
+  }
+
+
+  async googleLogin() {
+    const provider = new auth.GoogleAuthProvider();
+    const credential = await this.afAuth.signInWithPopup(provider);
+    return this.updateUserData(credential.user);
+  }
+
+  async nativeGoogleLogin(): Promise<any> {
+    const gplusUser = await this.gplus.login({
+      webClientId: 'ADD WEB CLIENT ID HERE!',
+      offline: true,
+      scopes: 'profile email'
+    });
+
+    return await this.afAuth.signInWithCredential(
+      auth.GoogleAuthProvider.credential(gplusUser.idToken)
+    );
+  }
+
+}
