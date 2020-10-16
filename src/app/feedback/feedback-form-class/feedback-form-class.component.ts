e27437c69c0f6e9a431507016f816d589e886020
jacobmusselman <musselman1989@gmail.com>
2020-10-16 14:56:13 -0700

    update to class and lesson feedback

--- src/app/feedback/feedback-form-class/feedback-form-class.component.ts
+++ src/app/feedback/feedback-form-class/feedback-form-class.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-import { ModalController } from '@ionic/angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ModalController, ToastController } from '@ionic/angular';
 import { DbService } from '../../services/db.service';
 import * as firebase from 'firebase';
 import { shareReplay } from 'rxjs/operators';
@@ -15,41 +15,31 @@ export class FeedbackFormClassComponent implements OnInit {
   
   feedbackForm: FormGroup;
   feedback;
-  lessons;
-  flights;
+  classes;
   
   constructor(
     private db: DbService,
     private modal: ModalController,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private toast: ToastController
   ) { }
   
   ngOnInit() {
-    this.feedbackForm = new FormGroup({
-      lesson: new FormControl(null, {
-        updateOn: "submit",
-        validators: [Validators.required]
-      }),
-      flight: new FormControl(null, {
-        updateOn: "submit",
-        validators: [Validators.required]
-      }),
-      comment: new FormControl(null, {
-        updateOn: "submit",
-        validators: [Validators.required, Validators.maxLength(300), Validators.minLength(1)]
-      })
+    const data = {
+      class: {},
+      ...this.feedback
+    };
+
+    this.feedbackForm = this.fb.group({
+      class: [ data.lesson,[Validators.required]],
+      comment: [ data.comment, [Validators.required, Validators.maxLength(300), Validators.minLength(1)]],
     });
 
-    this.lessons = this.db.collection$('lessons', ref =>
+    this.classes = this.db.collection$('classes', ref =>
     ref
       .orderBy('name', 'asc')
-    ), shareReplay(1); // only read the doc 1 if you subscribe to the observable multiple times
-
-    this.flights = this.db.collection$('flights', ref =>
-    ref
-      .orderBy('name', 'asc')
-    ), shareReplay(1); // only read the doc 1 if you subscribe to the observable multiple times
-  }
+    ), shareReplay(1); // only read the doc 1 if you subscribe to the observable multiple times.
+}
 
   async create() {
     const id = this.feedback ? this.feedback.id : '';
@@ -63,11 +53,25 @@ export class FeedbackFormClassComponent implements OnInit {
     this.modal.dismiss();
   }
 
-  trackByIdLesson(id, lesson) {
-    return lesson.id
+  trackById(id, cls) {
+    return cls.id;
   }
 
-  trackByIdFlight(id, flight) {
-    return flight.id
+  submitFeedback(){
+    this.presentToast();
+    this.clearForm();
+  }
+
+  async presentToast() {
+    const toast = await this.toast.create({
+      message: 'Thank you for your feedback!',
+      duration: 2000,
+      color: 'success'
+    });
+    toast.present();
+  }
+
+  clearForm() {
+    this.feedbackForm.reset();
   }
 }
